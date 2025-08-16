5ac11c8824ce0cdec755d1eb0b89b225f5ca1c90
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-16 10:42:58 +0530

    add choice feature

--- Task_1.java
+++ Task_1.java
@@ -12,6 +12,16 @@ public class Task_1 {
             System.out.println("Enter 4 to MULTIPLY ( * )");
             System.out.println("Enter 5 to DIVIDE   ( / )");
             System.out.println("Enter 6 to EXIT");
+            System.out.println("Choose an option (1-6)");
+
+            int choice;
+            if(sc.hasNextInt()){  //check's it is an integer or not
+                choice =  sc.nextInt();
+            }else{
+                System.out.println("Invalid input !!!");
+                System.out.println("Please enter a number between 1-6");
+                sc.next();   //clear's the buffer memory
+            }
         }
     }
 }
