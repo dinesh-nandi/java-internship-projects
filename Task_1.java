b4442fda00f07444c56de0dc262200b1a97b9fcd
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-16 11:12:20 +0530

    implemented switch case

--- Task_1.java
+++ Task_1.java
@@ -3,7 +3,11 @@ public class Task_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        double num1 = 0 , num2 = 0;
+        boolean operandsEntered = false;
+        double result;
         System.out.println("*** THE CALCULATOR ***");
+
         while(true){
             System.out.println("<---- MENU ---->");
             System.out.println("Enter 1 to enter OPERANDS");
@@ -21,6 +25,62 @@ public class Task_1 {
                 System.out.println("Invalid input !!!");
                 System.out.println("Please enter a number between 1-6");
                 sc.next();   //clear's the buffer memory
+                continue;    //skips the loop
+            }
+            switch(choice){
+                case 1:
+                    System.out.println("Enter the first number");
+                    num1 = sc.nextDouble();
+                    System.out.println("Enter the second number");
+                    num2 = sc.nextDouble();
+
+                    operandsEntered = true;
+                    break;
+
+                case 2:
+                    if(!operandsEntered){
+                        System.out.println("please enter operands first");
+                        break;
+                    }
+                    result = num1 + num2;
+                    System.out.println("The result is : " + result);
+                    break;
+
+                case 3:
+                    if(!operandsEntered){
+                        System.out.println("Please enter the operands first");
+                        break;
+                    }
+                    result = num1 - num2;
+                    System.out.println("The result is :" + result);
+                    break;
+
+                case 4:
+                    if(!operandsEntered){
+                        System.out.println("Please enter the operands first");
+                        break;
+                    }
+                    result = num1 * num2;
+                    System.out.println("The result is :" + result);
+                    break;
+
+                case 5:
+                    if(!operandsEntered){
+                        System.out.println("Please enter the operands first");
+                        break;
+                    }
+                    result = num1 / num2 ;
+                    System.out.println("The result is :" + result);
+                    break;
+
+                case 6:
+                    System.out.println("Exiting Calculator ....");
+                    System.out.println("Thank You for using it !!");
+                    sc.close();
+                    System.exit(0);
+
+                default:
+                    System.out.println("Please enter a valid option ( 1 -6 )");
             }
         }
     }
