adb92a566e1bb5ef100ee7e0882355f67c9f0b18
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-16 10:16:25 +0530

    add calculator menu

---
+++ Task_1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Task_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("*** THE CALCULATOR ***");
+        while(true){
+            System.out.println("<---- MENU ---->");
+            System.out.println("Enter 1 to enter OPERANDS");
+            System.out.println("Enter 2 to ADD      ( + )");
+            System.out.println("Enter 3 to SUBTRACT ( - )");
+            System.out.println("Enter 4 to MULTIPLY ( * )");
+            System.out.println("Enter 5 to DIVIDE   ( / )");
+            System.out.println("Enter 6 to EXIT");
+        }
+    }
+}
