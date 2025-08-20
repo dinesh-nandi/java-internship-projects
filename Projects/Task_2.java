b6f7cb19a209d4edbe13f1f0b367a970e2a6c1f4
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-20 22:43:56 +0530

    add menu section in bank management

---
+++ Projects/Task_2.java
@@ -0,0 +1,20 @@
+package Projects;
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+            System.out.println("****WELCOME TO THE BANK****");
+            System.out.println("Enter 1 to create Account");
+            System.out.println("Enter 2 to Deposit Money");
+            System.out.println("Enter 3 to Withdraw Money");
+            System.out.println("Enter 4 to Check Balance");
+            System.out.println("Enter 5 to Exit");
+
+            System.out.println("Enter your option ----> ");
+            
+        }
+    }
+}
