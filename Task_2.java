89a4342d9545513a2acd31534038bae5821a17f2
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-20 10:49:14 +0530

    Create basic menu for banking application

---
+++ Task_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Task_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true){
+            System.out.println("***WELCOME TO THE BANK***");
+            System.out.println("Enter 1 to Create Account");
+            System.out.println("Enter 2 to Deposit Money");
+            System.out.println("Enter 3 to Withdraw Money");
+            System.out.println("Enter 4 to Check Balance");
+            System.out.println("Enter 5 to Exit");
+
+            System.out.println("Enter your option --->");
+        }
+    }
+}
