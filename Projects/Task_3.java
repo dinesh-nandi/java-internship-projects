112ac8f834764a2b079a7b8645a48d08f1d092d1
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-23 10:05:57 +0530

    added a menu in bank management system

---
+++ Projects/Task_3.java
@@ -0,0 +1,22 @@
+package Projects;
+import java.util.Scanner;
+public class Task_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("**********************************");
+        System.out.println("<-- WELCOME TO STUDENT SECTION -->");
+        System.out.println("***********************************");
+
+        while(true){
+            System.out.println("Enter 1 to Add the Student");
+            System.out.println("Enter 2 to Update the Student");
+            System.out.println("Enter 3 to Remove the Student");
+            System.out.println("Enter 4 to View the Student Details");
+            System.out.println("Enter 5 to Exit");
+
+
+        }
+
+    }
+}
