d9b067984ea2bcd83bafedc2925c4e73a231755c
dinesh-nandi <dineshnandi07@gmail.com>
2025-08-23 13:55:03 +0530

    add student class

--- Projects/Task_3.java
+++ Projects/Task_3.java
@@ -1,8 +1,54 @@
 package Projects;
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Task_3 {
+    class Student{
+
+        private String Name;
+        private int Roll_No;
+        private int Age;
+        private String Department;
+
+        public Student(String name , int Roll_No , int age , String Department){
+            this.Name = Name;
+            this.Roll_No = Roll_No;
+            this.Age = Age;
+            this.Department = Department;
+        }
+
+        //getter and setter
+        public String getName() {
+            return Name;
+        }
+        public void setName(String name) {
+            this.Name = Name;
+        }
+
+        public int getRoll_No() {
+            return Roll_No;
+        }
+        public void setRoll_No(int roll_No) {
+            this.Roll_No = Roll_No;
+        }
+
+        public int getAge(){
+            return Age;
+        }
+        public void setAge(int age){
+            this.Age = Age;
+        }
+
+        public String getDepartment(){
+            return Department;
+        }
+        public void setDepartment(String department){
+           this.Department = Department;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        ArrayList<Student> Students = new ArrayList<>();
 
         System.out.println("**********************************");
         System.out.println("<-- WELCOME TO STUDENT SECTION -->");
@@ -19,7 +65,7 @@ public class Task_3 {
             int choice;
             if(sc.hasNextInt()){
                 choice = sc.nextInt();
-                break;
+                sc.nextLine(); // consume the line
             }else{
                 System.out.println("Invalid input!!");
                 System.out.println("Please re-enter from the above option");
